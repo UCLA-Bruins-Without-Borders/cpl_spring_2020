1e15eed1dacb7968d12002027c7e352dc3ccce57
bwbucla <64562900+bwbucla@users.noreply.github.com>
2020-06-28 17:10:06 -0700

    Added web dashboard

---
+++ app.js
@@ -0,0 +1,572 @@
+// puma_data is defined in data.js
+
+let clusters = [
+  [
+    [3706, "2016-2017"],
+    [3706, "2017-2018"],
+    [3708, "2016-2017"],
+    [3709, "2015-2016"],
+    [3709, "2017-2018"],
+    [3717, "2017-2018"],
+    [3721, "2016-2017"],
+    [3722, "2016-2017"],
+    [3723, "2015-2016"],
+    [3724, "2017-2018"],
+    [3725, "2015-2016"],
+    [3726, "2017-2018"],
+    [3729, "2016-2017"],
+    [3730, "2017-2018"],
+    [3731, "2015-2016"],
+    [3732, "2016-2017"],
+    [3733, "2017-2018"],
+    [3734, "2017-2018"],
+    [3741, "2016-2017"],
+    [3742, "2015-2016"],
+    [3742, "2017-2018"],
+    [3743, "2017-2018"],
+    [3745, "2017-2018"],
+    [3747, "2016-2017"],
+    [3749, "2017-2018"],
+    [3750, "2015-2016"],
+    [3751, "2015-2016"],
+    [3754, "2015-2016"],
+    [3757, "2016-2017"],
+    [3759, "2015-2016"],
+    [3768, "2016-2017"],
+  ],
+  [
+    [3705, "2015-2016"],
+    [3707, "2015-2016"],
+    [3707, "2016-2017"],
+    [3707, "2017-2018"],
+    [3708, "2015-2016"],
+    [3708, "2017-2018"],
+    [3709, "2016-2017"],
+    [3710, "2015-2016"],
+    [3710, "2016-2017"],
+    [3710, "2017-2018"],
+    [3717, "2015-2016"],
+    [3717, "2016-2017"],
+    [3718, "2015-2016"],
+    [3718, "2016-2017"],
+    [3718, "2017-2018"],
+    [3720, "2015-2016"],
+    [3720, "2017-2018"],
+    [3721, "2015-2016"],
+    [3722, "2017-2018"],
+    [3723, "2017-2018"],
+    [3724, "2016-2017"],
+    [3726, "2015-2016"],
+    [3726, "2016-2017"],
+    [3727, "2015-2016"],
+    [3727, "2017-2018"],
+    [3728, "2015-2016"],
+    [3728, "2016-2017"],
+    [3728, "2017-2018"],
+    [3729, "2015-2016"],
+    [3732, "2015-2016"],
+    [3732, "2017-2018"],
+    [3733, "2015-2016"],
+    [3734, "2016-2017"],
+    [3735, "2015-2016"],
+    [3735, "2016-2017"],
+    [3735, "2017-2018"],
+    [3736, "2015-2016"],
+    [3736, "2016-2017"],
+    [3736, "2017-2018"],
+    [3737, "2015-2016"],
+    [3737, "2016-2017"],
+    [3737, "2017-2018"],
+    [3738, "2015-2016"],
+    [3738, "2016-2017"],
+    [3738, "2017-2018"],
+    [3739, "2015-2016"],
+    [3739, "2016-2017"],
+    [3739, "2017-2018"],
+    [3740, "2015-2016"],
+    [3740, "2016-2017"],
+    [3740, "2017-2018"],
+    [3741, "2015-2016"],
+    [3741, "2017-2018"],
+    [3742, "2016-2017"],
+    [3743, "2016-2017"],
+    [3746, "2015-2016"],
+    [3747, "2017-2018"],
+    [3748, "2015-2016"],
+    [3748, "2016-2017"],
+    [3748, "2017-2018"],
+    [3749, "2015-2016"],
+    [3749, "2016-2017"],
+    [3752, "2015-2016"],
+    [3752, "2016-2017"],
+    [3752, "2017-2018"],
+    [3753, "2015-2016"],
+    [3753, "2016-2017"],
+    [3753, "2017-2018"],
+    [3754, "2016-2017"],
+    [3754, "2017-2018"],
+    [3755, "2015-2016"],
+    [3755, "2016-2017"],
+    [3755, "2017-2018"],
+    [3756, "2015-2016"],
+    [3756, "2016-2017"],
+    [3756, "2017-2018"],
+    [3757, "2017-2018"],
+    [3758, "2015-2016"],
+    [3758, "2016-2017"],
+    [3758, "2017-2018"],
+    [3759, "2016-2017"],
+    [3759, "2017-2018"],
+    [3760, "2015-2016"],
+    [3760, "2016-2017"],
+    [3760, "2017-2018"],
+    [3761, "2015-2016"],
+    [3761, "2016-2017"],
+    [3761, "2017-2018"],
+    [3762, "2015-2016"],
+    [3762, "2016-2017"],
+    [3762, "2017-2018"],
+    [3764, "2015-2016"],
+    [3764, "2016-2017"],
+    [3764, "2017-2018"],
+    [3767, "2016-2017"],
+    [3767, "2017-2018"],
+    [3768, "2015-2016"],
+    [3768, "2017-2018"],
+  ],
+  [
+    [3705, "2016-2017"],
+    [3705, "2017-2018"],
+    [3720, "2016-2017"],
+    [3721, "2017-2018"],
+    [3722, "2015-2016"],
+    [3723, "2016-2017"],
+    [3725, "2016-2017"],
+    [3727, "2016-2017"],
+    [3730, "2015-2016"],
+    [3730, "2016-2017"],
+    [3731, "2017-2018"],
+    [3733, "2016-2017"],
+    [3734, "2015-2016"],
+    [3744, "2016-2017"],
+    [3745, "2015-2016"],
+    [3746, "2016-2017"],
+    [3751, "2017-2018"],
+    [3767, "2015-2016"],
+  ],
+  [
+    [3729, "2017-2018"],
+    [3744, "2017-2018"],
+    [3745, "2016-2017"],
+    [3746, "2017-2018"],
+  ],
+  [
+    [3706, "2015-2016"],
+    [3724, "2015-2016"],
+    [3725, "2017-2018"],
+    [3731, "2016-2017"],
+    [3743, "2015-2016"],
+    [3744, "2015-2016"],
+    [3747, "2015-2016"],
+    [3750, "2016-2017"],
+    [3750, "2017-2018"],
+    [3751, "2016-2017"],
+    [3757, "2015-2016"],
+  ],
+];
+
+let map;
+let dict = new Map();
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 34.0522, lng: -118.2437 },
+    zoom: 10,
+  });
+
+  for (let i = 0; i < puma_data.length; i++) {
+    let curr = puma_data[i];
+
+    let polygon = new google.maps.Polygon({
+      paths: curr.coordinates[0],
+      strokeWeight: 1,
+      name: curr.name,
+      puma_id: curr.puma,
+      homelessness: curr.homeless,
+    });
+
+    polygon.setMap(map);
+
+    dict.set(curr.puma, polygon);
+
+    //attach event listeners to this polygon
+    google.maps.event.addListener(polygon, "click", function (e) {
+      dict.forEach((element) => {
+        element.setOptions({
+          strokeWeight: 1.0,
+          fillColor: "black",
+          fillOpacity: 0.4,
+        });
+      });
+      let content =
+        '<p class="county_name">' +
+        polygon.name +
+        "</p>" +
+        '<p class="puma"> PUMA: ' +
+        polygon.puma_id +
+        "</p>" +
+        '<div class="section">' +
+        '<div class="cluster"><p class="puma">Cluster timeframe:&nbsp;</p>' +
+        '<select id="cluster_select">' +
+        '<option value="2015-2016">2015-2016</option>' +
+        '<option value="2016-2017">2016-2017</option>' +
+        '<option selected="selected" value="2017-2018">2017-2018</option>' +
+        "</select></div>" +
+        '<div class="features">' +
+        '<p class="puma">Select year:&nbsp;</p>' +
+        '<select id="chart_select">' +
+        '<option value="2015">2015</option>' +
+        '<option value="2016">2016</option>' +
+        '<option value="2017">2017</option>' +
+        '<option value="2018">2018</option>' +
+        "</select>" +
+        '<p class="puma">&emsp;Feature:&nbsp;</p>' +
+        '<select id="feature_select">' +
+        '<option value="stores">Grocery Stores</option>' +
+        '<option value="dining">Dining Services</option>' +
+        '<option value="alcohol">Alcohol Services</option>' +
+        '<option value="libraries">Libraries</option>' +
+        '<option value="stops">Bus Stops</option>' +
+        '<option value="walked">Walked</option>' + // what is this??
+        '<option value="male">Male Population</option>' +
+        '<option value="female">Female Population</option>' +
+        "</select>" +
+        "</div>" +
+        "</div>" +
+        '<div class="section">' +
+        '<canvas id="chart1" width="350" height="350"></canvas>' +
+        '<canvas id="chart2" width="350" height="372"></canvas></div>';
+
+      let sidebar = document.getElementById("capture");
+      sidebar.innerHTML = content;
+
+      createLineChart(polygon.homelessness, "chart1", polygon.puma_id);
+
+      // selecting a year and feature to graph
+      let select = document.getElementById("chart_select");
+      let f_select = document.getElementById("feature_select");
+
+      let scatterChart;
+
+      select.addEventListener("change", (e) => {
+        if (scatterChart) scatterChart.destroy();
+        let cht2 = document.getElementById("chart2");
+
+        let year = parseInt(select.value) - 2015;
+        let feature = f_select.value;
+
+        let text = f_select.options[f_select.selectedIndex].text;
+
+        let point_colors = [];
+        let points = puma_data.map((element) => {
+          if (element.puma === polygon.puma_id) {
+            point_colors.push("rgba(242, 19, 109, 0.3)");
+          } else {
+            point_colors.push("rgba(0, 0, 0, 0.05)");
+          }
+
+          return {
+            x: element[feature][year],
+            y: element.homeless[year],
+          };
+        });
+
+        scatterChart = new Chart(cht2, {
+          type: "scatter",
+          data: {
+            datasets: [
+              {
+                label: text + " vs. Homeless Population",
+                data: points,
+                pointBackgroundColor: point_colors,
+              },
+            ],
+          },
+          options: {
+            title: {
+              display: true,
+              text:
+                "Plot of Key Features vs. Homeless Population for all PUMAs",
+            },
+            scales: {
+              xAxes: [
+                {
+                  type: "linear",
+                  position: "bottom",
+                  scaleLabel: {
+                    display: true,
+                    labelString: text,
+                  },
+                },
+              ],
+              yAxes: [
+                {
+                  scaleLabel: {
+                    display: true,
+                    labelString: "Homeless Population",
+                  },
+                },
+              ],
+            },
+            responsive: false,
+          },
+        });
+      });
+
+      f_select.addEventListener("change", (e) => {
+        if (scatterChart) scatterChart.destroy();
+        let cht2 = document.getElementById("chart2");
+
+        let year = parseInt(select.value) - 2015;
+        let feature = f_select.value;
+
+        let text = f_select.options[f_select.selectedIndex].text;
+
+        let point_colors = [];
+        let points = puma_data.map((element) => {
+          if (element.puma === polygon.puma_id) {
+            point_colors.push("rgba(242, 19, 109, 0.3)");
+          } else {
+            point_colors.push("rgba(0, 0, 0, 0.05)");
+          }
+
+          return {
+            x: element[feature][year],
+            y: element.homeless[year],
+          };
+        });
+
+        scatterChart = new Chart(cht2, {
+          type: "scatter",
+          data: {
+            datasets: [
+              {
+                label: text + " vs. Homeless Population",
+                data: points,
+                pointBackgroundColor: point_colors,
+              },
+            ],
+          },
+          options: {
+            title: {
+              display: true,
+              text:
+                "Plot of Key Features vs. Homeless Population for all PUMAs",
+            },
+            scales: {
+              xAxes: [
+                {
+                  type: "linear",
+                  position: "bottom",
+                  scaleLabel: {
+                    display: true,
+                    labelString: text,
+                  },
+                },
+              ],
+              yAxes: [
+                {
+                  scaleLabel: {
+                    display: true,
+                    labelString: "Homeless Population",
+                  },
+                },
+              ],
+            },
+            responsive: false,
+          },
+        });
+      });
+
+      // highlighting clusters
+      let puma_int = parseInt(polygon.puma_id);
+      let puma_timeframe = "2017-2018";
+
+      colorMap(puma_int, puma_timeframe, polygon);
+
+      let cluster_select = document.getElementById("cluster_select");
+
+      cluster_select.addEventListener("change", (e) => {
+        colorMap(puma_int, cluster_select.value, polygon);
+      });
+    });
+
+    // tooltip functions
+
+    google.maps.event.addListener(polygon, "mouseover", function (e) {
+      injectTooltip(e, polygon.name);
+    });
+
+    google.maps.event.addListener(polygon, "mousemove", function (e) {
+      moveTooltip(e);
+    });
+
+    google.maps.event.addListener(polygon, "mouseout", function (e) {
+      deleteTooltip(e);
+    });
+  }
+
+  console.log("done");
+}
+
+let colors = {
+  "2015-2016": "#5cab69",
+  "2016-2017": "#5382d4",
+  "2017-2018": "#eb9e34",
+};
+
+let strong_colors = {
+  "2015-2016": "#4fc96f",
+  "2016-2017": "#095beb",
+  "2017-2018": "#f5710c",
+};
+
+function colorMap(puma_int, puma_timeframe, polygon) {
+  for (let j = 0; j < clusters.length; j++) {
+    let res = clusters[j].find((e) => {
+      if (e[0] === puma_int && e[1] === puma_timeframe) return true;
+      else return false;
+    });
+    if (res) {
+      clusters[j].forEach((element) => {
+        let puma_str = "0" + element[0];
+        let puma_color = colors[element[1]];
+        dict.get(puma_str).setOptions({ fillColor: puma_color });
+      });
+      polygon.setOptions({
+        strokeWeight: 2.0,
+        fillColor: strong_colors[puma_timeframe],
+        fillOpacity: 0.6,
+      });
+      break;
+    }
+  }
+}
+
+function createLineChart(data, chart_id, puma_id) {
+  var cht1 = document.getElementById(chart_id);
+
+  var Chart1 = new Chart(cht1, {
+    type: "line",
+
+    data: {
+      labels: ["2015", "2016", "2017", "2018"], //x axis
+
+      datasets: [
+        {
+          label: "Homeless Population Count", //name of legend
+          fill: false, //if true, then the area under the curve will be highlighted
+          borderColor: "#000000", //The line color
+          borderDash: [0, 0], //makes the curve more or less checkered
+          backgroundColor: "#000000", //The line fill color
+          pointBackgroundColor: "#000000", //The fill color for points.
+          pointBorderColor: "#000000", //border color for points
+          pointHoverBackgroundColor: "#000000", //Point background color when hovered
+          pointHoverBorderColor: "#000000", //Point border color when hovered
+
+          data: data,
+        },
+      ],
+    },
+
+    options: {
+      title: {
+        display: true, //if false, then the title in the next line will not show
+        text: "Time vs. Homeless Population for PUMA " + puma_id,
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+      responsive: false,
+      // maintainAspectRatio: false,
+    },
+  });
+}
+
+//create a global variable that will point to the tooltip in the DOM
+var tipObj = null;
+
+//offset along x and y in px
+var offset = {
+  x: 20,
+  y: 20,
+};
+
+/********************************************************************
+ * injectTooltip(e,data)
+ * inject the custom tooltip into the DOM
+ ********************************************************************/
+var coordPropName = null;
+
+function injectTooltip(event, data) {
+  if (!tipObj && event) {
+    //create the tooltip object
+    tipObj = document.createElement("div");
+    tipObj.setAttribute("class", "tooltip");
+    tipObj.innerHTML = data;
+
+    //fix for the version issue
+    eventPropNames = Object.keys(event);
+    if (!coordPropName) {
+      //discover the name of the prop with MouseEvent
+      for (var i in eventPropNames) {
+        var name = eventPropNames[i];
+        if (event[name] instanceof MouseEvent) {
+          coordPropName = name;
+          console.log("--> mouse event in", coordPropName);
+          break;
+        }
+      }
+    }
+
+    if (coordPropName) {
+      //position it
+      tipObj.style.position = "fixed";
+      tipObj.style.top = event[coordPropName].clientY + offset.y + "px";
+      tipObj.style.left = event[coordPropName].clientX + offset.x + "px";
+
+      //add it to the body
+      document.body.appendChild(tipObj);
+    }
+  }
+}
+
+/********************************************************************
+ * moveTooltip(e)
+ * update the position of the tooltip based on the event data
+ ********************************************************************/
+function moveTooltip(event) {
+  if (tipObj && event && coordPropName) {
+    //position it
+    tipObj.style.top = event[coordPropName].clientY + offset.y + "px";
+    tipObj.style.left = event[coordPropName].clientX + offset.x + "px";
+  }
+}
+
+/********************************************************************
+ * deleteTooltip(e)
+ * delete the tooltip if it exists in the DOM
+ ********************************************************************/
+function deleteTooltip(event) {
+  if (tipObj) {
+    //delete the tooltip if it exists in the DOM
+    document.body.removeChild(tipObj);
+    tipObj = null;
+  }
+}
